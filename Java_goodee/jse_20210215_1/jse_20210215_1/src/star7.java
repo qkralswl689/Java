7aec77fc8fb1a845ed8d1080e7aaa0e09a7c98a7
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-15 22:05:19 +0900

    02.15

--- Java_goodee/jse_20210215_1/jse_20210215_1/src/star7.java
+++ Java_goodee/jse_20210215_1/jse_20210215_1/src/star7.java
@@ -2,7 +2,27 @@
 public class star7 {
 	public static void main(String[] args) {
 		
-	
+		for(int i=0; i<5; i++) { // 반복횟수
+			for(int k =7; k>=i*2; k--) { // k는 7부터 i*2 까지 1씩 감소하면서 반복
+				System.out.print(" ");
+			}
+			for(int j=0; j<=i*4;j++) { // j 는 0부터 i*4까지 1씩 증가하면서 반복한다
+				System.out.print("*");
+			}System.out.println();
+		
+		}
+		// 강사님 답안
+//		 for (int i=0; i<=4; i++) {
+//			 
+//	            // for (int j=0; j<=(5-i)*2-3; j++)
+//	            for (int j=0; j<=7-2*i; j++)
+//	                System.out.print(' ');
+//	 
+//	            for (int j=0; j<=4*i; j++)
+//	                System.out.print('*');
+//	 
+//	            System.out.println();
+//	        } //
 	}
 
 }
