5ab935af9ad7299f75fdf4993097ee2eef03258b
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-17 17:25:54 +0900

    02.17

---
+++ self_study/src/Java_Nam/Hello2.java
@@ -0,0 +1,11 @@
+package Java_Nam;
+
+// 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직 하다
+// 하나의 소스파일에는 하나의 public class만 허용한다
+// public class가 없는경우 소스파일 이름은 상관 X
+// 소스파일과 main메소드가 있는 클래스의 이름이 동일해야 한다
+public class Hello2 { // 소스파일의 이름은 public class이름과 일치해야한다
+	class Hello3{}
+	class Hello4{}
+
+}
