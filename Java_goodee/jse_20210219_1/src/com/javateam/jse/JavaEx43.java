2e51f7ed085b62f237674a02df9154d2ec4b8d74
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-19 17:49:39 +0900

    02.19

--- Java_goodee/jse_20210219_1/src/com/javateam/jse/JavaEx43.java
+++ Java_goodee/jse_20210219_1/src/com/javateam/jse/JavaEx43.java
@@ -2,7 +2,13 @@ package com.javateam.jse;
 
 public class JavaEx43 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		// Class clazz = String.class;
+		// Class clazz = new String().getClass();
+		Class clazz = Class.forName("java.lang.string");
+
+
 		
 	}
 
