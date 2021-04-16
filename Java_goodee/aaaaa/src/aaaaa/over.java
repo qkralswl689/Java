d0222ae8aa42b04f39a4f17424cd5c6b1ccdbac7
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-04-16 23:26:47 +0900

    04.16

---
+++ Java_goodee/aaaaa/src/aaaaa/over.java
@@ -0,0 +1,28 @@
+package aaaaa;
+
+public class over {
+	
+	public int a(int i) {
+		int k=3;
+		return i+k;
+	}
+
+public class over2 extends over{
+	
+		void a(String i){ //오버로딩
+			
+		} 
+		
+		public int a(int i) { // 오버라이딩
+						
+			return 3;
+		}
+		
+}
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
